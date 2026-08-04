adeb3779bad779e761da58dd37e05fe87c2b1916
Thang Hoang <hoangminhthang21051995@gmail.com>
2026-08-05 00:53:58 +0700

    Initial Commit

---
+++ qtool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  qtool
+//
+//  Created by Hoang Minh Thang on 5/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ qtool/qtoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  qtoolApp.swift
+//  qtool
+//
+//  Created by Hoang Minh Thang on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct qtoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
