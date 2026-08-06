d183fa5b2b8ec31d51247f9e1a27d61e656d3f7f
Thang Hoang <hoangminhthang21051995@gmail.com>
2026-08-06 22:58:57 +0700

    feat: init qtools

--- qtool/qtoolApp.swift
+++ qtool/qtoolApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import UIKit
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.portrait
+
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+}
 
 @main
 struct qtoolApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
